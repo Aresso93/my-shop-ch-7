8780033e956b4cee38a30a49c425e605c3ed5e1b
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-10-23 18:02:43 +0200

    fine del tutorial

--- src/pages/cms/products/components/CMSProductForm.tsx
+++ src/pages/cms/products/components/CMSProductForm.tsx
@@ -1,138 +1,116 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import { Product } from "@/model/product"
-import clsx from "clsx"
-
-declare var cloudinary: any;
+import clsx from 'clsx';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { Product } from '@/model/product';
+import { useCloudinary } from '../../../../shared/hooks/useCloudinary';
 
 export interface CMSProductFormProps {
-    activeItem: Partial<Product> | null;
-    onClose: () => void;
-    onAdd: (product: Partial<Product>) => void;
-    onEdit: (product: Partial<Product>) => void;
+  activeItem: Partial<Product> | null;
+  onClose: () => void;
+  onAdd: (product: Partial<Product>) => void;
+  onEdit: (product: Partial<Product>) => void;
 }
 
 const initialState: Partial<Product> = {
-    name: '', cost: 0, description: '', tmb: '', img: ''
+  name: '', cost: 0, description: '', tmb: '', img: ''
 }
 
-export function CMSProductForm(props: CMSProductFormProps){
-    const [formData, setFormData] = useState(initialState);
-    const [dirty, setDirty] = useState<boolean>(false);
+export function CMSProductForm(props: CMSProductFormProps) {
+  const [formData, setFormData] = useState(initialState);
+  const [dirty, setDirty] = useState<boolean>(false);
 
-    useEffect(() => {
+  const {openWidget} = useCloudinary()
 
-        if (props.activeItem?.id) {
-            setFormData({...props.activeItem})
-        } else {
-            setFormData(initialState)
-        }
-    }, [props.activeItem])
-
-    function changeHandler(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
-        const val = event.currentTarget.value;
-        const name = event.currentTarget.name;
-        setFormData(state => ({...state, [name]: val}));
-        setDirty(true);
+  useEffect(() => {
+    if (props.activeItem?.id) {
+      setFormData({ ...props.activeItem })
+    } else {
+      setFormData(initialState)
     }
+  }, [props.activeItem])
 
-    function saveHandler(event: FormEvent<HTMLFormElement>){
-        event.preventDefault();
-        if(props.activeItem?.id){
-            props.onEdit(formData);
-        } else {
-            props.onAdd(formData);
-            
-        }
-        
+  function changeHandler(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const value = e.currentTarget.value;
+    const name = e.currentTarget.name;
+    setFormData(s => ({ ...s, [name]: value }));
+    setDirty(true);
+  }
+
+  function saveHandler(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (props.activeItem?.id) {
+      props.onEdit(formData);
+    } else {
+      props.onAdd(formData);
     }
-    
-    function uploadHandler(){
-        const uploadWidget = cloudinary.openUploadWidget(
-            {
-                cloudName: 'dho3eksv0',
-                uploadPreset: 'ml_default',
-                sources: ['local', 'url', 'camera']
-            },
-            function(error: any, result: any) {
-                if (!error && result.event === 'success') {
-                    const img = result.info.url;
-                    const tmb = result.info.thumbnail_url;
-                    setFormData(state => ({...state, img, tmb}))
-                }
-                
-            }
-        )
+  }
 
-        uploadWidget.open();
-    }
+  function uploadHandler(){
+    openWidget()
+    .then(res =>{
+        setFormData(state => ({...state, ...res}))
+    })
+  }
 
-    const isNameValid = formData.name?.length;
-    const isCostValid = formData.cost! > 0; 
-    const isDescriptionValid = formData.description?.length;
-    const isValid = isNameValid && isCostValid && isDescriptionValid 
-    
+  const isNameValid = formData.name?.length;
+  const isCostValid = formData.cost! > 0;
+  const isDescValid = formData.description?.length;
 
-    return (
+  const isValid = isNameValid && isCostValid && isDescValid;
+
+
+  return (
     <div className={clsx(
-        "fixed bg-slate-200 z-10 text-black top-0 w-96 h-full transition-all overflow-auto",
-        {'-right-96': !props.activeItem, 'right-0': props.activeItem}
+      'fixed bg-slate-200 z-10 text-black top-0 w-96  h-full transition-all overflow-auto',
+      {'-right-96': !props.activeItem, 'right-0': props.activeItem}
     )}>
 
-        <form onSubmit={saveHandler}>
-
+      <form onSubmit={saveHandler}>
         <div className="flex justify-around h-16">
-            <button 
-                className="text-white w-1/2 bg-green-500 hover:bg-green-600 disabled:opacity-30"
-                disabled={!isValid}
-                type="submit"
-                >SAVE
-            </button>
-            <button 
-                className="text-white w-1/2 bg-slate-500 hover:bg-slate-600"
-                onClick={props.onClose}
-                type="button"
-                >CLOSE</button>
+          <button
+            className="text-white w-1/2 bg-green-500 hover:bg-green-600 disabled:opacity-30"
+            disabled={!isValid}
+            type="submit"
+          >SAVE</button>
+          <button
+            className="text-white w-1/2 bg-slate-500 hover:bg-slate-600"
+            onClick={props.onClose}
+            type="button"
+          >CLOSE</button>
         </div>
 
         {
-            formData.img &&
-            <img src={formData.img} alt={formData.name} className="w-full"/>
+          formData.img &&
+          <img src={formData.img} alt={formData.name} className="w-full" />
         }
 
         <div className="flex flex-col gap-3 mx-3 mt-16">
-            Product Name
-            <input
-            className={clsx({'error': !isNameValid && dirty})} 
-            type="text" 
-            value={formData?.name}
-            name="name"
-            onChange={changeHandler}/>
+          Product Name:
+          <input
+            className={clsx({ 'error': !isNameValid && dirty})}
+            type="text" value={formData?.name} name="name" onChange={changeHandler}
+          />
 
-            Product Cost
-            <input
-            className={clsx({'error': !isCostValid && dirty})} 
-            type="number" 
-            value={formData?.cost}
-            name="cost"
-            onChange={changeHandler}/>
-            Description
-            <textarea
-            className={clsx({'error': !isDescriptionValid && dirty})}
-            value={formData.description}
-            name="description"
-            onChange={changeHandler}
-            ></textarea>
+          Product Cost:
+          <input
+            className={clsx({ 'error': !isCostValid && dirty})}
+            type="number" value={formData?.cost} name="cost" onChange={changeHandler}
+          />
 
+          Description
+          <textarea
+            className={clsx({ 'error': !isDescValid && dirty})}
+            value={formData.description} name="description" onChange={changeHandler}
+          ></textarea>
+
+          <button className="btn primary" type="button" onClick={uploadHandler}>
+            UPLOAD IMAGE
+          </button>
         </div>
+      </form>
 
-        <button className="btn primary" onClick={uploadHandler}>
-            Upload Picture
-        </button>
+      <pre>{JSON.stringify(formData, null, 2)}</pre>
 
-        </form>
 
-        <pre>{JSON.stringify(formData, null, 2)}</pre>
-        
     </div>
-    )
+  )
 }
--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,9 +1,9 @@
 import { useCart, useCartPanel } from '@/services/cart';
 import { useEffect, useState } from 'react';
-import { pb } from '../../pocketbase';
 import { Product } from '@/model/product';
 import { ServerError, Spinner } from '@/shared/';
 import { ProductCard } from './components/ProductCard';
+import { useProductsService } from '@/services/products';
 
 
 export function ShopPage() {
@@ -12,35 +12,26 @@ export function ShopPage() {
   const [error, setError] = useState<boolean>(false);
 
   const openCartPanel = useCartPanel(state => state.openOverlay);
-  const addToCart = useCart(state => state.addToCart)
+  const addToCart = useCart(state => state.addToCart);
+
+  const {actions, state} = useProductsService();
 
   useEffect(() => {
-    loadData()
+    actions.getProducts();
   }, [])
 
-  function loadData() {
-    setError(false);
-    setPending(true);
 
-    pb.collection('products').getList<Product>()
-      .then(res => {
-        setError(false);
-        setProducts(res.items);
-      })
-      .catch(() => setError(true))
-      .finally(() => setPending(false))
-  }
 
   return (
     <div>
       <h1 className="title">SHOP</h1>
 
-      {pending && <Spinner />}
-      {error && <ServerError />}
+      {state.pending && <Spinner />}
+      {state.error && <ServerError />}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
       {
-        products.map(p => {
+        state.products.map(p => {
           return (
             <ProductCard
               key={p.id}
